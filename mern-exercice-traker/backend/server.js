d2043122c6b01b22e3366e6d7f0079f9ebf12d1f
d4rcel <daraola05@gmail.com>
2023-04-07 09:58:52 +0100

    Fix

--- mern-exercice-traker/backend/server.js
+++ mern-exercice-traker/backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const exercisesRouter = require('./routes/exercises')
+const usersRouter = require('./routes/users')
+
 mongoose.set("strictQuery", false);
 
 require("dotenv").config();
@@ -8,6 +11,7 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors())
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
@@ -20,6 +24,9 @@ const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
+
+app.use('/exercises', exercisesRouter)
+app.use('/users', usersRouter)
 
 app.listen(port, () => {
   console.log(`Server is running on port : ${port}`);
